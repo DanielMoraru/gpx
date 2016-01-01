e9c7d5d0cea7be9ecb5459543e66e281d6f61883
haudegen07 <daniel.moraru@hotmail.de>
2016-01-01 21:32:41 +0100

    gpxreader erweitert

--- gpx/src/main/java/de/dmu/gpx/gpx/ReadGpx.java
+++ gpx/src/main/java/de/dmu/gpx/gpx/ReadGpx.java
@@ -5,8 +5,10 @@
  */
 package de.dmu.gpx.gpx;
 
-import com.topografix.gpx._1._0.Gpx;
 import java.io.File;
+import java.io.InputStream;
+import java.io.Reader;
+import java.net.URL;
 import javax.xml.bind.JAXB;
 import javax.xml.bind.JAXBException;
 
@@ -16,7 +18,43 @@ import javax.xml.bind.JAXBException;
  */
 public class ReadGpx {
 
-    public Gpx readGpx(File file) throws JAXBException {
-        return JAXB.unmarshal(file, Gpx.class);
+    public com.topografix.gpx._1._0.Gpx readGpx10(File file) throws JAXBException {
+        return JAXB.unmarshal(file, com.topografix.gpx._1._0.Gpx.class);
+    }
+    
+    public com.topografix.gpx._1._0.Gpx readGpx10(InputStream inputStream) throws JAXBException {
+        return JAXB.unmarshal(inputStream, com.topografix.gpx._1._0.Gpx.class);
+    }
+    
+    public com.topografix.gpx._1._0.Gpx readGpx10(Reader reader) throws JAXBException {
+        return JAXB.unmarshal(reader, com.topografix.gpx._1._0.Gpx.class);
+    }
+    
+    public com.topografix.gpx._1._0.Gpx readGpx10(String string) throws JAXBException {
+        return JAXB.unmarshal(string, com.topografix.gpx._1._0.Gpx.class);
+    }
+    
+    public com.topografix.gpx._1._0.Gpx readGpx10(URL url) throws JAXBException {
+        return JAXB.unmarshal(url, com.topografix.gpx._1._0.Gpx.class);
+    }
+    
+    public com.topografix.gpx._1._1.GpxType readGpx11(File file) throws JAXBException {
+        return JAXB.unmarshal(file, com.topografix.gpx._1._1.GpxType.class);
+    }
+    
+    public com.topografix.gpx._1._1.GpxType readGpx11(InputStream inputStream) throws JAXBException {
+        return JAXB.unmarshal(inputStream, com.topografix.gpx._1._1.GpxType.class);
+    }
+    
+    public com.topografix.gpx._1._1.GpxType readGpx11(Reader reader) throws JAXBException {
+        return JAXB.unmarshal(reader, com.topografix.gpx._1._1.GpxType.class);
+    }
+    
+    public com.topografix.gpx._1._1.GpxType readGpx11(String string) throws JAXBException {
+        return JAXB.unmarshal(string, com.topografix.gpx._1._1.GpxType.class);
+    }
+    
+    public com.topografix.gpx._1._1.GpxType readGpx11(URL url) throws JAXBException {
+        return JAXB.unmarshal(url, com.topografix.gpx._1._1.GpxType.class);
     }
 }
